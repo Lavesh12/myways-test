1b1e0d52e55d6811c4e7bb2326f9646c16cfbb35
Lavesh12 <94188971+Lavesh12@users.noreply.github.com>
2023-04-13 18:21:45 +0530

    Create lavesh.js

---
+++ lavesh.js
@@ -0,0 +1,21 @@
+function fizzBuzzPhone(phoneNumber) {
+  let sum = 0;
+  let output = '';
+  for (let i = 0; i < phoneNumber.length; i++) {
+    sum += parseInt(phoneNumber[i]);
+  }
+  for (let i = 1; i <= sum; i++) {
+    if (i % 4 === 0 && i % 5 === 0) {
+      output += 'FizzBuzz ';
+    } else if (i % 4 === 0) {
+      output += 'Fizz ';
+    } else if (i % 5 === 0) {
+      output += 'Buzz ';
+    } else {
+      output += i + ' ';
+    }
+  }
+  return output.trim();
+}
+
+console.log(fizzBuzzPhone('7987724718'));
